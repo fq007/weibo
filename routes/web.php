1e543655f78bf579f4ccd2674d62b39bd05d28e4
fq007 <1291530566@qq.com>
2021-09-09 09:34:44 +0800

    用户注册

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,11 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+/*
+ * 注册  start
+ */
+Route::get("signup","UserController@create")->name('signup');
+/*
+ * 注册 end
+ */
