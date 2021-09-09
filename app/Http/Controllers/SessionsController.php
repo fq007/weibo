49ba2382347b7cee23905d540cf227cb23ac8e5b
fq007 <1291530566@qq.com>
2021-09-09 14:29:13 +0800

    添加登录时记住我功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,7 +21,7 @@ class SessionsController extends Controller
            'email'     => 'required|email|max:255',
             'password' => 'required'
         ]);
-        if(Auth::attempt($result)){
+        if(Auth::attempt($result,$request->has('remember'))){
             session()->flash('success','欢迎回来');
             return redirect()->route('user.show',[Auth::user()]);
         }else{
