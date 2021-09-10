3008a1425c1e0670d34fc0d6d6df3c5fdd4fb0df
fq007 <1291530566@qq.com>
2021-09-10 16:40:24 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -100,4 +100,10 @@ class User extends Authenticatable
         }
         $this->followings()->detach($user_ids);
     }
+
+    public function feed(){
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids,$this->id);
+        return Status::whereIn('user_id',$user_ids)->with('user')->orderBy('created_at','desc');
+    }
 }
